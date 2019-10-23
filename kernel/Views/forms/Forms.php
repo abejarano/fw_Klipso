fe2f788575d8d8afdffc53cd26c1aa1019be0618
angel <angel@alphacode.com.br-at-503688510464>
2019-10-23 09:12:22 -0300

    recibe nombre del modelo, instancia y obtiende los campos del modelo para generar form

--- kernel/Views/forms/Forms.php
+++ kernel/Views/forms/Forms.php
@@ -7,12 +7,37 @@
  */
 
 
-namespace fw_Klipso\kernel\Views;
+namespace fw_Klipso\kernel\Views\forms;
 
 
 class Forms
 {
+    private static $_path_app = '';
+    private static $_model = '';
+    private static  $_form = [];
+    private static $_fields_model = [];
+
+    public static function setPathApp($path) {
+        Forms::$_path_app = $path;
+        
+    }
+
     public static function setModel($model) {
+        Forms::$_model = ucfirst($model);
+        Forms::__loadFieldMoel();
+    }
+
+    private static function __loadFieldMoel() {
+        # instanciar la clave model
+        $path_model = Forms::$_path_app . '\\models\\' .Forms::$_model;
+
+        if (!class_exists($path_model)) {
+            pr('El modelo ' . Forms::$_model . ' NO existe en su aplicación');
+        }
+        
+        $objModel = new $path_model;
+
+        Forms::$_fields_model = $objModel->__fields__();
 
     }
 }
